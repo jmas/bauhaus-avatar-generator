7556c24f2d4ea9e53477973b4fdfc92c581f6cff
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2025-09-23 08:17:13 +0300

    Update cf-worker

--- cf-worker/src/index.ts
+++ cf-worker/src/index.ts
@@ -4,6 +4,11 @@ export default {
   async fetch(request, env, ctx): Promise<Response> {
     const url = new URL(request.url);
 
+    // Handle favicon.svg requests by passing them to the server
+    if (url.pathname === "/favicon.svg") {
+      return fetch(request);
+    }
+
     const pathParts = url.pathname.split("/");
     const filename = pathParts.pop(); // abcd123.svg
     const [id, extension] = filename?.split(".") ?? []; // id = "abcd123", ext = "svg"
